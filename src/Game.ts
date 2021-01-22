dee51482f836c08eb06a5dd6ef014070b88b5381
simon2669 <feltalalo26@gmail.com>
2021-01-22 21:58:37 +0100

    Game by Simon

--- src/Game.ts
+++ src/Game.ts
@@ -16,9 +16,18 @@ import BaseGame from './BaseGame';
  * osztályban a hiányzó metódusokat. Úgy találod meg őket, hogy abstract 
  * metódusként vannak definiálva.
  */
-export default class Game extends BaseGame {
+export default class Game extends BaseGame implements IBaseGame {
+  head: Piece;
+  tail: Piece;
+  food: Piece | null;
+  goldenApple: Piece | null;
+  length: number;
+  growth: number;
+  score: number;
+  currentLevel: Level | null;
+  garden: HTMLDivElement;
 
-  constructor (private levels: Level[]) {
+  constructor(private levels: Level[]) {
     super();
     this.head = new Piece({ x: 80, y: 80, type: 'head' });
     this.tail = this.resetHead();
@@ -28,15 +37,60 @@ export default class Game extends BaseGame {
     this.setEvents();
   }
 
-  get highScore (): number {
+  // /**
+  //    * @returns {number}
+  //    * Egy random számot szorozz meg a this.level.length -el, 
+  //    * majd kerekítsd lefelé, ez lesz az index.
+  //    * Majd térj vissza a this.levels tömbnek ezzel az indexével.
+  //    */
+  // abstract getRandomLevel(): Level 
+  //!Miért nem number?
+  getRandomLevel(): Level {
+    let index = Math.floor(Math.random() * this.levels.length);
+    return this.levels[index];
+  }
+
+  //   /**
+  //    * @returns {boolean}
+  //    * 1. hozz létre egy chance neű változót 5 értékkel
+  //    * 2. hozz létre egy pick nevű változót, értéke random szám szorozva 100 -al
+  //    * 3. térj vissza true értékkel, ha a pick kisebb int a chance
+  //    */
+  mayIHaveGoldenApple(): boolean {
+    let chance = 5;
+    let pick = Math.random() * 100;
+    return chance > pick ? true : false;
+  }
+
+  //   /**
+  //    * @returns {void}
+  //    * A metódus feladatai:
+  //    * 1. keresd meg a DOM -ban az összes .vertical-grid és .horizontal-grid 
+  //    * elemet
+  //    * 2. mentsd el őket egy grids nevű változóba
+  //    * 3. járd be a tömböt, és minden elemére hívd meg a Utils.removeNode 
+  //    * metódust, hogy eltávolítsd őket az oldalról
+  //    * 4. a this.gridVisible értékét állítsd false -ra
+  //    */
+  removeGrid(): void {
+    let grids = document.querySelectorAll('.vertical-grid , .horizontal-grid');
+
+    grids.forEach((item: HTMLElement) => {
+      Utils.removeNode(item);
+    });
+
+    this.gridVisible = false;
+  }
+
+  get highScore(): number {
     return parseInt(localStorage.getItem('high-score') || '0', 10) || 0;
   }
 
-  set highScore (value: number) {
+  set highScore(value: number) {
     localStorage.setItem('high-score', value.toString());
   }
 
-  renderGarden () {
+  renderGarden() {
     const { clientHeight, clientWidth } = document.body;
     const TOP = Math.max(60, Math.floor(clientHeight * 0.10));
     const LEFT = Math.max(60, Math.floor(clientWidth * 0.10));
@@ -63,10 +117,10 @@ export default class Game extends BaseGame {
     this.showScore();
   }
 
-  
+
 
   // Remove the old chain, put HEAD in the starting position
-  resetHead (): Piece {
+  resetHead(): Piece {
     if (this.head.next) {
       this.head.next.remove();
       this.head.next = null;
@@ -89,7 +143,7 @@ export default class Game extends BaseGame {
   /**
    * Reset all values and restart the game
    */
-  start (): void {
+  start(): void {
     // Don"t restart already running game
     if (this.moving === false) {
       this.tail = this.resetHead();
@@ -108,7 +162,7 @@ export default class Game extends BaseGame {
   /**
    * GAME OVER
    */
-  over (): void {
+  over(): void {
     this.moving = false;
     // const { score } = this;
 
@@ -123,14 +177,14 @@ export default class Game extends BaseGame {
     this.splashToggle(true);
   }
 
-  showTopScore () {
+  showTopScore() {
     const top = document.getElementById('top') as HTMLDivElement;
     this.highScore = this.highScore < this.score ? this.score : this.highScore;
     top.innerHTML = `TOP: ${this.highScore}`;
   }
 
   // eslint-disable-next-line class-methods-use-this
-  splashToggle (show: boolean) {
+  splashToggle(show: boolean) {
     const splash = document.querySelector('.splash') as HTMLElement;
     splash.style.display = show ? '' : 'none';
   }
@@ -138,7 +192,7 @@ export default class Game extends BaseGame {
   /**
    * Get a random empty location for food
    */
-  getFoodLocation (): number[] {
+  getFoodLocation(): number[] {
     let x = Utils.rand(MARGIN, this.garden.clientWidth - MARGIN, SIZE);
     let y = Utils.rand(MARGIN, this.garden.clientHeight - MARGIN, SIZE);
 
@@ -152,7 +206,7 @@ export default class Game extends BaseGame {
     return [x, y];
   }
 
-  handleFood (): void {
+  handleFood(): void {
     // If the there is no food, create a random one.
     if (this.food == null) {
       const [foodX, foodY] = this.getFoodLocation();
@@ -177,16 +231,16 @@ export default class Game extends BaseGame {
     }
   }
 
-  
 
-  handleGoldenApple () {
+
+  handleGoldenApple() {
     if (this.goldenApple === null) {
       const [foodX, foodY] = this.getFoodLocation();
       this.goldenApple = new Piece({ x: foodX, y: foodY, type: 'golden' });
     }
   }
 
-  async swallowFood (type: string) {
+  async swallowFood(type: string) {
     if (type === 'food') {
       if (this.food == null) { return; }
       this.tail.next = this.food;
@@ -226,14 +280,14 @@ export default class Game extends BaseGame {
     }
   }
 
-  getSpeed (): number {
+  getSpeed(): number {
     const initialSpeed = 200;
     const calculated = (initialSpeed - this.growth * 0.5) + this.debugSpeed + this.keyHeld;
 
     return Utils.bound(calculated, FASTEST, SLOWEST);
   }
 
-  updateScore (won: number): number {
+  updateScore(won: number): number {
     if (this.noClip === true) {
       return this.score;
     }
@@ -243,14 +297,14 @@ export default class Game extends BaseGame {
     return this.score;
   }
 
-  showScore (): void {
+  showScore(): void {
     const points = document.getElementById('points') as HTMLDivElement;
 
     // Speed: ${Math.floor(1000 / this.getSpeed())}bps
     points.innerHTML = `${this.score}`;
   }
 
-  frame (): void {
+  frame(): void {
     if (this.moving) {
       setTimeout(() => {
         requestAnimationFrame(this.frame.bind(this));
@@ -318,19 +372,19 @@ export default class Game extends BaseGame {
    * Don"t let snake to go backwards
    */
   // eslint-disable-next-line class-methods-use-this
-  notBackwards (key: number): boolean {
+  notBackwards(key: number): boolean {
     const lastDirection = Directions.peek();
 
     if ((lastDirection === keys.UP && key === keys.DOWN)
-        || (lastDirection === keys.DOWN && key === keys.UP)
-        || (lastDirection === keys.LEFT && key === keys.RIGHT)
-        || (lastDirection === keys.RIGHT && key === keys.LEFT)) {
+      || (lastDirection === keys.DOWN && key === keys.UP)
+      || (lastDirection === keys.LEFT && key === keys.RIGHT)
+      || (lastDirection === keys.RIGHT && key === keys.LEFT)) {
       return false;
     }
     return true;
   }
 
-  setEvents (): void {
+  setEvents(): void {
     document.addEventListener('keydown', (e: KeyboardEvent) => {
       switch (e.keyCode) {
         // Toggle Grid
@@ -350,7 +404,7 @@ export default class Game extends BaseGame {
         case keys.J:
           this.debugSpeed += 10;
           break;
-          // Speed up the snake
+        // Speed up the snake
         case keys.K:
           this.debugSpeed -= 10;
           break;
@@ -438,9 +492,9 @@ export default class Game extends BaseGame {
     }, 100));
   }
 
-  
 
-  drawGrid (): void {
+
+  drawGrid(): void {
     for (let x = 0; x < this.garden.clientWidth; x += SIZE) {
       const div = document.createElement('div');
       div.style.top = '0px';
@@ -460,7 +514,7 @@ export default class Game extends BaseGame {
     this.gridVisible = true;
   }
 
-  drawHitboxes () {
+  drawHitboxes() {
     document.querySelectorAll('.hitbox').forEach(Utils.removeNode);
 
     Locations.getAll().forEach((a, k) => {
